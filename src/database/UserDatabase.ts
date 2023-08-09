128f9e80688cbbf5260e8672de32969e477d764d
Francine Hahn <102267713+francinehahn@users.noreply.github.com>
2023-08-09 17:01:13 -0300

    Update UserDatabase.ts

--- src/database/UserDatabase.ts
+++ src/database/UserDatabase.ts
@@ -1,11 +1,10 @@
-import { log } from "console";
 import { UserRepository } from "../business/UserRepository"
-import { UserDTO } from "../models/User";
+import { CreateUserDTO } from "../models/User"
 import BaseDatabase from "./BaseDatabase"
 
 export default class UserDatabase extends BaseDatabase implements UserRepository {
     TABLE_NAME = "rpg_users"
-    public createUser = async (user:UserDTO):Promise<any> => {
+    public createUser = async (user:CreateUserDTO):Promise<any> => {
         try {
           const create =  await UserDatabase.connection.insert({
                id: user.id,
